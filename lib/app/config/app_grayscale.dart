318934c9a7e41b86655f21402171494d2a35bec1
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-05-02 16:02:25 +0900

    feat(#3): 누락된 Grayscale 파일 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ lib/app/config/app_grayscale.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppGrayscale {
+  static const Color gray99 = Color(0xFFFCFBFC);
+  static const Color gray95 = Color(0xFFF6F5F6);
+  static const Color gray90 = Color(0xFFF2F1F2);
+  static const Color gray85 = Color(0xFFE8E7EA);
+  static const Color gray80 = Color(0xFFDAD7DD);
+  static const Color gray70 = Color(0xFFC1BDC5);
+  static const Color gray60 = Color(0xFFA6A3A9);
+  static const Color gray55 = Color(0xFF948E99);
+  static const Color gray50 = Color(0xFF8E8E93);
+  static const Color gray40 = Color(0xFF636366);
+  static const Color gray30 = Color(0xFF48484A);
+  static const Color gray20 = Color(0xFF3A3A3C);
+  static const Color gray10 = Color(0xFF2D2634);
+}
