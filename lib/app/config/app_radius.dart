4ee3ad655c1c6996929cb8806f6e5c845c26ffd6
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-05-02 14:52:03 +0900

    feat: Design Const들 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ lib/app/config/app_radius.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppRadius {
+  static BorderRadius none = BorderRadius.zero;
+  static BorderRadius xxsmall = BorderRadius.circular(6.r);
+  static BorderRadius xsmall = BorderRadius.circular(8.r);
+  static BorderRadius small = BorderRadius.circular(12.r);
+  static BorderRadius medium = BorderRadius.circular(14.r);
+  static BorderRadius large = BorderRadius.circular(16.r);
+  static BorderRadius xlarge = BorderRadius.circular(20.r);
+  static BorderRadius xxlarge = BorderRadius.circular(24.r);
+  static BorderRadius full = BorderRadius.circular(400.r);
+}
