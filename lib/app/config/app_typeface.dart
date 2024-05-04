22aead3c43eddc7cd6bc11ea94ab0856eeaaea3c
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-05-04 18:26:20 +0900

    refactor(#8): Typeface 기본 색상 설정
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/app/config/app_typeface.dart
+++ lib/app/config/app_typeface.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'app_color.dart';
+
 class AppTypeface {
   // Title
-  final TextStyle title28Medium = TextStyle(fontSize: 28.sp, fontWeight: FontWeight.w500, height: 1.57);
-  final TextStyle title26Semibold = TextStyle(fontSize: 26.sp, fontWeight: FontWeight.w600, height: 1.38);
+  final TextStyle title28Medium = TextStyle(fontSize: 28.sp, fontWeight: FontWeight.w500, height: 1.57, color: AppColor.black);
+  final TextStyle title26Semibold = TextStyle(fontSize: 26.sp, fontWeight: FontWeight.w600, height: 1.38, color: AppColor.black);
 
   // Headline
-  final TextStyle headline24Bold = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700, height: 1.5);
-  final TextStyle headline24Medium = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w500, height: 1.41);
+  final TextStyle headline24Bold = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700, height: 1.5, color: AppColor.black);
+  final TextStyle headline24Medium = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w500, height: 1.41, color: AppColor.black);
 
   // Body
-  final TextStyle body20Bold = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w700, height: 1.5);
-  final TextStyle body18Bold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700, height: 1.55);
-  final TextStyle body18Semibold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600, height: 1.55);
+  final TextStyle body20Bold = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w700, height: 1.5, color: AppColor.black);
+  final TextStyle body18Bold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700, height: 1.55, color: AppColor.black);
+  final TextStyle body18Semibold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600, height: 1.55, color: AppColor.black);
 
   // Label
-  final TextStyle label16Semibold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600, height: 1.625);
-  final TextStyle label14Medium = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, height: 1.57);
-  final TextStyle label12Bold = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700, height: 1.5);
-  final TextStyle label12Regular = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w400, height: 1.5);
+  final TextStyle label16Semibold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600, height: 1.625, color: AppColor.black);
+  final TextStyle label14Medium = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, height: 1.57, color: AppColor.black);
+  final TextStyle label12Bold = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700, height: 1.5, color: AppColor.black);
+  final TextStyle label12Regular = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w400, height: 1.5, color: AppColor.black);
 }
