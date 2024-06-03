b219a15243e20f9676cf511f4fd5668db19a137a
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-06-03 22:23:58 +0900

    feat(#10): AppTheme 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/app/config/app_theme.dart
+++ lib/app/config/app_theme.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ticats/app/config/app_grayscale.dart';
 
 class AppTheme {
   static final ThemeData defaultTheme = ThemeData(
     fontFamily: 'SUIT',
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+      iconTheme: const IconThemeData(color: Colors.black),
+      scrolledUnderElevation: 0,
+      titleSpacing: 0,
+      toolbarHeight: 54.h,
+    ),
+    scaffoldBackgroundColor: AppGrayscale.gray85,
   );
 }
